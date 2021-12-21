45d03fdda59ec2b1f72f9fcb90b174040bf64b36
Robin Guyomar <robin.guyomar@etu.univ-lehavre.r>
2021-12-21 20:10:49 +0100

    Obtention de l'API + implémentation de Google Maps + Affichage d'une carte en mode suiveur

--- SuiviDeParcours/app/src/main/java/com/example/suivideparcours/Suiveur.java
+++ SuiviDeParcours/app/src/main/java/com/example/suivideparcours/Suiveur.java
@@ -1,12 +1,18 @@
 package com.example.suivideparcours;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
 
-import androidx.appcompat.app.AppCompatActivity;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class Suiveur extends AppCompatActivity {
+public class Suiveur extends AppCompatActivity implements OnMapReadyCallback {
 
     TextView tv;
 
@@ -20,6 +26,18 @@ public class Suiveur extends AppCompatActivity {
 
         tv = findViewById(R.id.numero);
         tv.setText("Suivi de : "+numeroMarcheur);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        googleMap.addMarker(new MarkerOptions()
+                .position(new LatLng(0, 0))
+                .title("Marker"));
     }
 }
+
 
