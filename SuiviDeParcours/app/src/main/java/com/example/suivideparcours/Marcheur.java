5d60bf221d7142b990dc776a48d35823575d3498
Robin Guyomar <robin.guyomar@etu.univ-lehavre.r>
2021-12-28 17:42:51 +0100

    Correction mineure

--- SuiviDeParcours/app/src/main/java/com/example/suivideparcours/Marcheur.java
+++ SuiviDeParcours/app/src/main/java/com/example/suivideparcours/Marcheur.java
@@ -14,7 +14,9 @@ import android.hardware.SensorManager;
 import android.location.Location;
 import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.telephony.SmsManager;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -185,17 +187,33 @@ public class Marcheur extends AppCompatActivity
     public void onMapReady(GoogleMap googleMap) {
         map = googleMap;
         verifyPermissions();
-        locationActivated();
-        map.setOnMyLocationChangeListener(this);
-    }
 
-    public void locationActivated() {
-        if(myLatitude==0 && myLongitude==0) {
+        if(!isLocationEnabled(Marcheur.this)){
             AlertDialog.Builder builder = new AlertDialog.Builder(Marcheur.this);
             builder.setMessage("Veuillez activer votre localisation");
             builder.setPositiveButton("OK", (dialog, which) -> dialog.cancel());
             AlertDialog dialog = builder.create();
             dialog.show();
+        }
+
+        map.setOnMyLocationChangeListener(this);
+    }
+
+
+    public static boolean isLocationEnabled(Context context) {
+        int locationMode = 0;
+        String locationProviders;
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            try {
+                locationMode = Settings.Secure.getInt(context.getContentResolver(), Settings.Secure.LOCATION_MODE);
+            } catch (Settings.SettingNotFoundException e) {
+                e.printStackTrace();
+            }
+            return locationMode != Settings.Secure.LOCATION_MODE_OFF;
+        } else {
+            locationProviders = Settings.Secure.getString(context.getContentResolver(), Settings.Secure.LOCATION_MODE);
+            return !TextUtils.isEmpty(locationProviders);
         }
     }
 
