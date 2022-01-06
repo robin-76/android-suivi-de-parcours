7b635a7a6db35a8dc9b0e7e9846ed39088c89c44
Robin Guyomar <robin.guyomar@etu.univ-lehavre.r>
2022-01-06 18:17:43 +0100

    Ajout de la fonctionnalité de screenshot pour le suiveur

--- SuiviDeParcours/app/src/main/java/com/example/suivideparcours/Suiveur.java
+++ SuiviDeParcours/app/src/main/java/com/example/suivideparcours/Suiveur.java
@@ -1,12 +1,25 @@
 package com.example.suivideparcours;
 
-import androidx.appcompat.app.AppCompatActivity;
+import static android.Manifest.permission.READ_EXTERNAL_STORAGE;
+import static android.Manifest.permission.READ_SMS;
+import static android.Manifest.permission.RECEIVE_SMS;
+import static android.Manifest.permission.SEND_SMS;
+import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
 
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.telephony.SmsManager;
 import android.view.View;
@@ -23,6 +36,8 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.Polyline;
 import com.google.android.gms.maps.model.PolylineOptions;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -31,6 +46,7 @@ import java.util.List;
 public class Suiveur extends AppCompatActivity
         implements OnMapReadyCallback {
 
+    private static final int PERMISSION_FOR_STORAGE = 0;
     TextView tv;
     GoogleMap map;
     Polyline polyline;
@@ -48,6 +64,9 @@ public class Suiveur extends AppCompatActivity
 
         tv = findViewById(R.id.numero);
         tv.setText("Suivi de : "+numeroMarcheur);
+
+        if (!checkPermission())
+            verifyPermissions();
 
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
@@ -108,6 +127,57 @@ public class Suiveur extends AppCompatActivity
                 .geodesic(true);
 
         polyline = map.addPolyline(polylineOpts);
+    }
+
+    private boolean checkPermission() {
+        int permission1 = ContextCompat.checkSelfPermission(getApplicationContext(), READ_EXTERNAL_STORAGE);
+        int permission2 = ContextCompat.checkSelfPermission(getApplicationContext(), WRITE_EXTERNAL_STORAGE);
+        return permission1 == PackageManager.PERMISSION_GRANTED && permission2 == PackageManager.PERMISSION_GRANTED;
+    }
+
+    public void verifyPermissions() {
+        if((ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED)
+                || (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED))
+            ActivityCompat.requestPermissions(this, new String[]{
+                    Manifest.permission.READ_EXTERNAL_STORAGE,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE}, PERMISSION_FOR_STORAGE);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int code, String permissions[], int grantResults[]) {
+        switch (code) {
+            case PERMISSION_FOR_STORAGE: {
+                boolean ok = true;
+                if(grantResults.length > 0) {
+                    for(int res=0; res<grantResults.length; res++) {
+                        if(grantResults[res] != PackageManager.PERMISSION_GRANTED)
+                            ok = false;
+                    }
+                } else ok = false;
+                if(ok) {
+                    Toast.makeText(this, "Stockage autorisé", Toast.LENGTH_LONG).show();
+                }
+                else {
+                    Toast.makeText(this, "Stockage non autorisé", Toast.LENGTH_LONG).show();
+                }
+            }
+        }
+    }
+
+    public void buttonScreen(View v) {
+        map.setOnMapLoadedCallback(() -> map.snapshot(bitmap -> {
+            FileOutputStream out = null;
+            try {
+                out = new FileOutputStream("/mnt/sdcard/map.png");
+                Toast.makeText(Suiveur.this,"Image enregistrée",
+                        Toast.LENGTH_LONG).show();
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+            bitmap.compress(Bitmap.CompressFormat.PNG, 90, out);
+        }));
     }
 
     public void buttonStop(View v) {
